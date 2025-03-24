713075bea08bf9c4e3eca7e9025c570ecde5c826
Fırat Alçın <firatalcin04@gmail.com>
2025-03-24 23:10:07 +0300

    Variables, Naming Variables, Operators, Implicity Typed Variables

---
+++ UltimateCSharpMasterclass/01_CSharpFundamentals/Program.cs
@@ -0,0 +1,35 @@
+﻿//Variables
+
+string name = "Nisha";
+int number;
+number = 5;
+number = 10;
+Console.WriteLine(number);
+
+//Naming Variables
+
+//string class = "First"; // keywords'ler değişken ismi olamazlar
+string @class = "First";
+int _number = 10;
+int also_some_number = 5;
+int alsoSomeNumber = 5;
+//int 1number = 1; // değişken isimleri sayı ile başlayamaz
+string name1 = "Anna";
+int age, Age;
+
+//Operators
+
+int sum = 5 + 10;
+int resultNoParenthesis = 5 + 2 * 3;
+int resultParenthesis = 5 + (2 * 3);
+var stringAndInt = "abc" + 5;
+
+//Implicity Typed Variables
+
+string word1 = "text";
+var word2 = "text";
+//var invalidVariables; // var kullanılırken ilk değer değişken tanımlanırken verilmelidir.
+
+
+
+
