bfed1ff381418c2283f1f0889a187a111066b663
Fırat Alçın <firatalcin04@gmail.com>
2025-05-05 23:07:38 +0300

    Fields vs Properties, Static

--- UltimateCSharpMasterclass/02_BasicOfOOP/Program.cs
+++ UltimateCSharpMasterclass/02_BasicOfOOP/Program.cs
@@ -97,3 +97,62 @@ class Rectangle
     public string NotUsingAnyState() => "abc";
 }
 
+
+class MedicalAppointment
+{
+    private string _patientName;
+    private DateTime _date;
+
+    public MedicalAppointment(string patientName, DateTime date)
+    {
+        _patientName = patientName;
+        _date = date;
+    }
+
+    //calling one constructor from another
+    //commented now, as the below constructor with optional parameters
+    //also allows to skip the second parameter
+    //public MedicalAppointment(string patientName) :
+    //    this(patientName, 7)
+    //{
+    //}
+
+    public MedicalAppointment(
+        string patientName = "Unknown", int daysFromNow = 7)
+    {
+        _patientName = patientName;
+        _date = DateTime.Now.AddDays(daysFromNow);
+    }
+
+    public DateTime GetDate() => _date;
+
+    public void Reschedule(DateTime date)
+    {
+        _date = date;
+        var printer = new MedicalAppointmentPrinter();
+        printer.Print(this);
+    }
+
+    public void OverwriteMonthAndDay(int month, int day)
+    {
+        _date = new DateTime(_date.Year, month, day);
+    }
+
+    public void MoveByMonthsAndDays(int monthsToAdd, int daysToAdd)
+    {
+        _date = new DateTime(
+            _date.Year,
+            _date.Month + monthsToAdd,
+            _date.Day + daysToAdd);
+    }
+}
+
+class MedicalAppointmentPrinter
+{
+    public void Print(MedicalAppointment medicalAppointment)
+    {
+        Console.WriteLine(
+            "Appointment will take place on " + medicalAppointment.GetDate());
+    }
+}
+
