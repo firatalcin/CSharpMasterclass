a0310e6b40f7ca4fecaae87eff156abc6693bd22
Fırat Alçın <firatalcin04@gmail.com>
2026-01-12 23:24:50 +0300

    DiceRollGame

---
+++ UltimateCSharpMasterclass/02_DiceRollGame/Game/Dice.cs
@@ -0,0 +1,18 @@
+﻿namespace _02_DiceRollGame.Game
+{
+    public class Dice
+    {
+        private readonly Random _random;
+        private const int SidesCount = 6;
+
+        public Dice(Random random)
+        {
+            _random = random;
+        }
+
+        public int Roll()
+        {
+            return _random.Next(1, SidesCount + 1);
+        }
+    }
+}
---
+++ UltimateCSharpMasterclass/02_DiceRollGame/Game/GuessingGame.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02_DiceRollGame.Game
+{
+    public class GuessingGame
+    {
+        private readonly Dice _dice;
+        private const int InitialTries = 3;
+
+        public GuessingGame(Dice dice)
+        {
+            _dice = dice;
+        }
+
+        public GameResult Play()
+        {
+            var diceRollResult = _dice.Roll();
+            Console.WriteLine(
+               $"Dice rolled. Guess what number it shows in {InitialTries} tries.");
+
+            var triesLeft = InitialTries;
+            while (triesLeft > 0)
+            {
+                Console.WriteLine($"You have {triesLeft} tries left. Enter your guess (1-6):");
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if(guess == diceRollResult)
+                {
+                    return GameResult.Victory;
+                }
+
+                Console.WriteLine("Wrong number.");
+                --triesLeft;
+            }
+            return GameResult.Loss;
+        }
+
+        public static void PrintResult(GameResult gameResult)
+        {
+            if (gameResult == GameResult.Victory)
+            {
+                Console.WriteLine("Congratulations! You've guessed the correct number!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you've used all your tries. Better luck next time!");
+            }
+        }
+    }
+}
